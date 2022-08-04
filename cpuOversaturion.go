e15e636ddce4d9f4e625b91a127817d34557212b
Hoang Do <hoang.dv@tripi.vn>
2022-08-04 11:13:03 +0700

    update

--- cpuOversaturion.go
+++ cpuOversaturion.go
@@ -11,6 +11,29 @@ import (
 	"time"
 )
 
+type Metric struct {
+	Cluster   string `json:"cluster"`
+	Instance  string `json:"instance"`
+	Job       string `json:"job"`
+	Namespace string `json:"namespace"`
+	Pod       string `json:"pod"`
+}
+
+type Result struct {
+	Metric Metric        `json:"metric"`
+	Value  []interface{} `json:"value"`
+}
+
+type Data struct {
+	ResultType string   `json:"resultType"`
+	Result     []Result `json:"result"`
+}
+
+type Response struct {
+	Status string `json:"status"`
+	Data   Data   `json:"data"`
+}
+
 func GetCpuOversaturation() []model.CpuOversaturion {
 	cpuOversaturationResponse := make([]model.CpuOversaturion, 0)
 	cluster := GetCluster()
